9b02828a89466e0676662d34e8d6c83d20ab4221
AbdullahLaham <abed26194@gmail.com>
2022-10-05 13:29:06 +0300

    adding the like functionality on the post

--- components/Post.js
+++ components/Post.js
@@ -6,15 +6,47 @@ import {FiShare2} from 'react-icons/fi'
 import {TbBrandGoogleAnalytics} from 'react-icons/tb'
 import {FiTrash2} from 'react-icons/fi';
 import Moment from 'react-moment';
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Post = ({post}) => {
+  // the current user
+  const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('user')));
+  const [likes, setLikes] = useState([]);
+  const [isLiked, setIsLiked] = useState(false);
+  // adding like to the post function
+  const addLike = async () => {
+
+    if (isLiked) {
+      await deleteDoc(doc(db, "posts", post.id, "likes", currentUser?._id));
+    } else {
+      await setDoc(doc(db, "posts", post.id, "likes", currentUser?._id), {
+        username: currentUser?.userName,
+      });
+    }
+
+  }
+  useEffect(() => {
+    const unsubscribe = onSnapshot(query(collection(db, "posts", post?.id, "likes")), (snapshot) => {
+      setLikes(snapshot.docs);
+      console.log('unsubscribe', snapshot.docs)
+    });
+    
+  }, [db])
+  useEffect(() => {
+
+    setIsLiked(likes.findIndex((like) => like.id === currentUser?._id) !== -1);
+    console.log('isLiked', post?.id ,  isLiked)
+  }, [likes, currentUser])
   return (
     <div className='flex justify-start mb-[2rem] p-[1rem]'>
       <div className='mr-[.5rem]'>
         <img src={post?.data()?.userImg} className='w-[4rem] h-[4rem] rounded-full object-cover'  />
       </div>
       <div>
-        <div className='flex items-center justify-between'><div className='flex items-center whitespace-nowrap'><p className='mr-[.5rem] cursor-pointer font-bold'>{post?.data()?.name}</p><p className='mr-[.5rem]'>{post?.data()?.username}</p><p className='mr-[.5rem] hover:underline hover:cursor-pointer text-sm '>- <Moment fromNow>{post.data().timestamp.toDate()}</Moment></p> </div><div className='p-[.5rem] text-3xl ml-[3rem] hoverAnimation cursor-pointer'><CgMoreAlt className='' /></div></div>
+        <div className='flex items-center justify-between'><div className='flex items-center whitespace-nowrap'><p className='mr-[.5rem] cursor-pointer font-bold'>{post?.data()?.name}</p><p className='mr-[.5rem]'>{post?.data()?.username}</p><p className='mr-[.5rem] hover:underline hover:cursor-pointer text-sm '>- <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment></p> </div><div className='p-[.5rem] text-3xl ml-[3rem] hoverAnimation cursor-pointer'><CgMoreAlt className='' /></div></div>
         <p className='text-gray-800 text-[15px] sm:text-[16px] '>{post?.data()?.text}</p>
         <div className='max-h-[25rem] overflow-hidden mt-[1.1rem]'>
             {post.data().image && <img src={post?.data()?.image} className='h-[30rem] w-[30rem] object-cover rounded-md' />}
@@ -26,9 +58,12 @@ const Post = ({post}) => {
             <div className='text-xl cursor-pointer hoverAnimation p-[1rem] hover:bg-red-200 hover:text-red-500'>
                 <FiTrash2 className='cursor-pointer'/>
             </div>
-            <div className='text-xl cursor-pointer hoverAnimation p-[1rem] hover:bg-red-200 hover:text-red-500'>
+            {<div>
+              <div className={`flex items-center gap-1 text-xl cursor-pointer hoverAnimation p-[1rem] ${isLiked && "text-red-500 bg-red-200 active:text-black active:bg-white"}` }onClick={addLike}>
                 <BsFillSuitHeartFill className='cursor-pointer'/>
-            </div>
+                <p className='mt-[-.2rem]'>{likes.length > 0 && likes.length}</p>
+              </div>
+            </div>}
             <div className='text-xl cursor-pointer hoverAnimation p-[1rem] hover:bg-blue-200 hover:text-blue-500'>
                 <FiShare2 className='cursor-pointer'/>
             </div>
